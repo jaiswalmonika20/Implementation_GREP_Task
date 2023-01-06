b2601be17fa0f7204bc7cfe7064efbe4654787bc
jaiswalmonika20 <49835078+jaiswalmonika20@users.noreply.github.com>
2023-01-06 15:47:55 +0530

    single and multiple flag implementd

--- GREP_Implementation_task/Grep/Search.go
+++ GREP_Implementation_task/Grep/Search.go
@@ -11,7 +11,11 @@ import (
 func Search(pattern string, flag, selectfile []string) []string {
 	output := []string{}
 	for _, flagno := range flag {
-		output = append(output, chooseflag(flagno, pattern, selectfile)...)
+		if len(flagno)==2{
+		output = append(output, choosesingleflag(flagno, pattern, selectfile)...)
+		} else{
+			output=append(output,choosemultipleflag(pattern,flagno,file)...)
+		}
 	}
 	return output
 }
@@ -28,7 +32,7 @@ func filelines(files string) []string {
 	return content
 }
 
-func chooseflag(flag, pattern string, files []string) []string {
+func choosesingleflag(flag, pattern string, files []string) []string {
 	result := []string{}
 	if flag == "-n" {
 		for _, fileno := range files {
@@ -48,7 +52,6 @@ func chooseflag(flag, pattern string, files []string) []string {
 			for _, line := range filecontent {
 				status = 0
 				if strings.Contains(line, pattern) {
-					fmt.Println("bye")
 					status = 1
 					break
 				}
@@ -88,5 +91,97 @@ func chooseflag(flag, pattern string, files []string) []string {
 			}
 		}
 	}
+
 	return result
+}
+
+func choosemultipleflag(pattern string,flag,files []string,)[]string{
+	result:=[]string{}
+	mapflags:=map[string]int{"-n":0,"-l":0,"-i":0,"-v":0,"-x":0}
+	for _ ,choosenflag := range flag{
+		mapflags[choosenflag]=1
+	}
+
+	if (mapflags["-n"]==1 && mapflags["-i"]==1){
+		for _, fileno := range files {
+			filecontent := filelines(fileno)
+			for index, line := range filecontent{
+				if strings.Contains(strings.ToLower(line), strings.ToLower(pattern))
+				linenumber := strconv.Itoa(index + 1)
+					result = append(result, fileno+":"+linenumber+"-"+pattern)
+
+			}
+		}
+	} else if (mapflags["-l"] == 1 ; mapflags["-n"] == 1){
+		for _, fileno := range files {
+			filecontent := filelines(fileno)
+			for index, line := range filecontent{
+				status = 0
+				if strings.Contains(line, pattern) {
+					fmt.Println("bye")
+					status = 1
+					break
+				}
+			}
+			if status == 1 {
+				result = append(result, fileno)
+			}
+			
+		}
+	} else if (mapflags["-x"]==1 && mapflags["-n"]==1){
+		for _, fileno := range files {
+			filecontent := filelines(fileno)
+			for index, line := range filecontent{
+				if pattern == line {
+					linenumber := strconv.Itoa(index + 1)
+					result = append(result, fileno+":"+linenumber+"-"+pattern)
+				}
+
+
+			}
+		}
+	} else if (mapflags["-l"]==1 && mapflags["-i"]==1){
+		for _, fileno := range files {
+			filecontent := filelines(fileno)
+			for index, line := range filecontent{
+				status = 0
+				if strings.Contains(strings.ToLower(line), strings.ToLower(pattern)) {
+					status = 1
+					break
+				}
+			}
+			if status == 1 {
+				result = append(result, fileno)
+			}
+		}
+	} else if (mapflags["-l"]==1 && mapflags["-x"]==1){
+		for _, fileno := range files {
+			filecontent := filelines(fileno)
+			for index, line := range filecontent{
+				status=0
+				if pattern == line {
+					status = 1
+					break
+				}
+			}
+			if status == 1 {
+				result = append(result, fileno)
+			}
+		}
+}else if (mapflags["-i"]==1 && mapflags["-x"]==1){
+	for _, fileno := range files {
+		filecontent := filelines(fileno)
+		for index, line := range filecontent{
+			if pattern == line{
+				if strings.Contains(strings.ToLower(line), strings.ToLower(pattern)) {
+					result = append(result, line)
+			}
+		}
+		}
+	}
+} else {
+	result=append(result,[]string{})
+
+}
+return result
 }
