63425910ed985b81ed6afac2c8f5913f00a76f78
jaiswalmonika20 <49835078+jaiswalmonika20@users.noreply.github.com>
2023-01-06 10:52:32 +0530

    Add files via upload

--- GREP_Implementation_task/main.go
+++ GREP_Implementation_task/main.go
@@ -1,29 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"grepimplementation/search/Grep"
+	"os"
+	"strings"
 )
 
 func main() {
-	//fmt.Println("Grep implementation task")
-	//fmt.Println("enter the pattern:")
-	//reader := bufio.NewReader(os.Stdin)
-	//pattern, _ := reader.ReadString('\n')
+	fmt.Println("Grep implementation task")
+	fmt.Println("enter the pattern:")
+	reader := bufio.NewReader(os.Stdin)
+	pattern, _ := reader.ReadString('\n')
 
-	//var flag []string
-	//fmt.Println("flags choosen to check the pattern:")
-	//flaginput, _ := reader.ReadString('\n')
-	//flag = append(flag, flaginput)
-
-	//var selectfile []string
-	// fmt.Println("flies choosen to check the pattern:")
-	// fileinput, _ := reader.ReadString('\n')
-	// selectfile = append(selectfile, fileinput)
-	// var output []string
-	pattern := "hello"
-	flag := []string{"-n", "-i"}
-	selectfile := []string{"input1.txt", "input2.txt"}
+	var flag []string
+	fmt.Println("flags choosen to check the pattern:")
+	flaginput, _ := reader.ReadString('\n')
+	for _, token := range strings.Split(flaginput, " ") {
+		flag = append(flag, token)
+		fmt.Println(flag)
+	}
+	var selectfile []string
+	fmt.Println("files choosen to check the pattern:")
+	fileinput, _ := reader.ReadString('\n')
+	for _, token := range strings.Split(fileinput, " ") {
+		selectfile = append(selectfile, token)
+		fmt.Println(selectfile)
+	}
+	//var output []string
+	// pattern := "hello"
+	// flag := []string{"-n", "-i"}
+	// selectfile := []string{"input1.txt", "input2.txt"}
 	output := Grep.Search(pattern, flag, selectfile)
 	fmt.Println(output)
 
