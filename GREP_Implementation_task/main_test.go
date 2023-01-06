4eda16666d14c15a7190c1e7c945cbcb7a6b8b64
jaiswalmonika20 <49835078+jaiswalmonika20@users.noreply.github.com>
2023-01-06 11:58:07 +0530

    Add files via upload

--- GREP_Implementation_task/main_test.go
+++ GREP_Implementation_task/main_test.go
@@ -1,72 +1,60 @@
 package main
 
 import (
-	"fmt"
 	"grepimplementation/search/Grep"
 	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_Search(t *testing.T) {
-	file := []string{"input1.txt", "input2.txt"}
-	flags := []string{"-n", "-l", "-i", "-v", "-x"}
+	t.Run("Zero case testing", Test_CaseZero)
+	t.Run("first case testing", Test_CaseOne)
+	t.Run("second case testing", Test_CaseTwo)
+	t.Run("third case testing", Test_CaseThree)
+	t.Run("fourth case testing", Test_CaseFour)
+}
 
-	//not selecting a flag
-	fmt.Println("test case 1")
-	if len(flags) == 0 {
-		actual1 := Grep.Search("hello", []string{}, file)
-		expected1 := []string{"hello", "input1.txt"}
-		t.Errorf("actual result(%s) is not equal to expected result(%s)", actual1, expected1)
-	}
+// not selecting a flag
+func Test_CaseZero(t *testing.T) {
+	actual := Grep.Search("hello", []string{}, []string{"input1.txt"})
+	expected := []string{"hello", "input1.txt"}
+	assert.Equal(t, reflect.DeepEqual(actual, expected), "the actual and expected result not match")
+}
 
-	//for flag -n
-	fmt.Println("test case 2")
-	actual2 := Grep.Search("hello", []string{"-n"}, file)
-	expected2 := []string{"input1.txt:1-hello", "input1.txt:2-hello world", "input1.txt:3-hello world again", "input2.txt:1-hello"}
-	if reflect.DeepEqual(actual2, expected2) {
-		t.Logf("actual result(%s) is equal to expected result(%s)", actual2, expected2)
-	} else {
-		t.Errorf("actual result(%s) is not equal to expected result(%s)", actual2, expected2)
-	}
+// for flag -n
+func Test_CaseOne(t *testing.T) {
+	actual := Grep.Search("hello", []string{"-n"}, []string{"input1.txt"})
+	expected := []string{"input1.txt:1-hello", "input1.txt:2-hello world", "input1.txt:3-hello world again", "input2.txt:1-hello"}
+	assert.Equal(t, reflect.DeepEqual(actual, expected), "the actual and expected result not match")
+}
 
-	//for flag -l
-	fmt.Println("test case 3")
+// for flag -l
+func Test_CaseTwo(t *testing.T) {
 
-	actual3 := Grep.Search("hello", []string{"-l"}, file)
-	expected3 := []string{"input1.txt", "input2.txt"}
-	if reflect.DeepEqual(actual3, expected3) {
-		t.Logf("actual result(%s) is equal to expected result(%s)", actual3, expected3)
-	} else {
-		t.Errorf("actual result(%s) is not equal to expected result(%s)", actual3, expected3)
-	}
+	actual := Grep.Search("hello", []string{"-l"}, []string{"input1.txt"})
+	expected := []string{"input1.txt", "input2.txt"}
+	assert.Equal(t, reflect.DeepEqual(actual, expected), "the actual and expected result not match")
+}
 
-	//for flag -i
-	fmt.Println("test case 4")
-	actual4 := Grep.Search("heLlO", []string{"-i"}, file)
-	expected4 := []string{"hello", "hello world", "hello world again", "HelloAGAin", "hello"}
-	if reflect.DeepEqual(actual3, expected3) {
-		t.Logf("actual result(%s) is equal to expected result(%s)", actual4, expected4)
-	} else {
-		t.Errorf("actual result(%s) is not equal to expected result(%s)", actual4, expected4)
-	}
+// for flag -i
+func Test_CaseThree(t *testing.T) {
+	actual := Grep.Search("heLlO", []string{"-i"}, []string{"input1.txt"})
+	expected := []string{"hello", "hello world", "hello world again", "HelloAGAin", "hello"}
+	assert.Equal(t, reflect.DeepEqual(actual, expected), "the actual and expected result not match")
+}
 
-	//for flag -v
-	fmt.Println("test case 5")
-	actual5 := Grep.Search("hello", []string{"-v"}, file)
-	expected5 := []string{"HelloAGAin", "again", "hi my name", "hi my name is monika"}
-	if reflect.DeepEqual(actual3, expected3) {
-		t.Logf("actual result(%s) is equal to expected result(%s)", actual5, expected5)
-	} else {
-		t.Errorf("actual result(%s) is not equal to expected result(%s)", actual5, expected5)
-	}
+// for flag -v
+func Test_CaseFour(t *testing.T) {
+	actual := Grep.Search("hello", []string{"-v"}, []string{"input1.txt"})
+	expected := []string{"HelloAGAin", "again", "hi my name", "hi my name is monika"}
+	assert.Equal(t, reflect.DeepEqual(actual, expected), "the actual and expected result not match")
+}
 
-	//for flag -x
-	fmt.Println("test case 6")
-	actual6 := Grep.Search("hello", []string{"-x"}, file)
-	expected6 := []string{"hello", "hello"}
-	if reflect.DeepEqual(actual3, expected3) {
-		t.Logf("actual result(%s) is equal to expected result(%s)", actual6, expected6)
-	} else {
-		t.Errorf("actual result(%s) is not equal to expected result(%s)", actual6, expected6)
-	}
+// for flag -x
+func Test_CaseFive(t *testing.T) {
+	actual := Grep.Search("hello", []string{"-x"}, []string{"input1.txt"})
+	expected := []string{"hello", "hello"}
+	assert.Equal(t, reflect.DeepEqual(actual, expected), "the actual and expected result not match")
 }
