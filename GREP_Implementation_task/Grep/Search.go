ab6a61e28f29bed05864195e19248ae015729316
jaiswalmonika20 <49835078+jaiswalmonika20@users.noreply.github.com>
2023-01-06 10:53:48 +0530

    Add files via upload

--- GREP_Implementation_task/Grep/Search.go
+++ GREP_Implementation_task/Grep/Search.go
@@ -2,6 +2,7 @@ package Grep
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
@@ -9,38 +10,26 @@ import (
 
 func Search(pattern string, flag, selectfile []string) []string {
 	output := []string{}
-
 	for _, flagno := range flag {
-
-		//output = chooseflag(flagno, pattern, selectfile)
-
 		output = append(output, chooseflag(flagno, pattern, selectfile)...)
-
 	}
-
 	return output
-
 }
 
 func filelines(files string) []string {
-
 	file, _ := os.Open(files)
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 	var content []string
-
 	for scanner.Scan() {
 		content = append(content, scanner.Text())
 	}
 	file.Close()
 	return content
-
 }
 
 func chooseflag(flag, pattern string, files []string) []string {
-
 	result := []string{}
-
 	if flag == "-n" {
 		for _, fileno := range files {
 			filecontent := filelines(fileno)
@@ -50,11 +39,8 @@ func chooseflag(flag, pattern string, files []string) []string {
 					result = append(result, fileno+":"+linenumber+"-"+pattern)
 				}
 			}
-
 		}
-
 	}
-
 	if flag == "-l" {
 		for _, fileno := range files {
 			var status int
@@ -62,57 +48,45 @@ func chooseflag(flag, pattern string, files []string) []string {
 			for _, line := range filecontent {
 				status = 0
 				if strings.Contains(line, pattern) {
+					fmt.Println("bye")
 					status = 1
 					break
 				}
 			}
 			if status == 1 {
 				result = append(result, fileno)
-
 			}
 		}
 	}
-
 	if flag == "-i" {
-
 		for _, fileno := range files {
 			filecontent := filelines(fileno)
 			for _, line := range filecontent {
 				if strings.Contains(strings.ToLower(line), strings.ToLower(pattern)) {
-
 					result = append(result, line)
 				}
 			}
 		}
-
 	}
-
 	if flag == "-v" {
 		for _, fileno := range files {
 			filecontent := filelines(fileno)
 			for _, line := range filecontent {
 				if !strings.Contains(line, pattern) {
-
 					result = append(result, line)
 				}
-
 			}
 		}
-
 	}
-
 	if flag == "-x" {
-
 		for _, fileno := range files {
 			filecontent := filelines(fileno)
 			for _, line := range filecontent {
 				if pattern == line {
 					result = append(result, line)
 				}
-
 			}
 		}
-
 	}
 	return result
 }
